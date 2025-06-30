dba5591d0cd102e73d865ddd4e8df6cd7d7cb35b
srisara1409 <srisara1409@gmail.com>
2025-06-30 19:28:55 +1000

    config and file upload changes

--- src/main/java/com/ms/userServices/services/RegisterService.java
+++ src/main/java/com/ms/userServices/services/RegisterService.java
@@ -5,22 +5,37 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
-
-import com.ms.userServices.repository.VehicleRepository;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class RegisterService {
 
-	@Autowired
-	private VehicleRepository repo;
+//	@Autowired
+//	private VehicleRepository repo;
+	
+	private final String baseUploadDir = "uploads";
 
-	public byte[] generateBankDetailsPdf(String accountName, String bsbNumber, String accountNumber, String financialInstName) throws Exception {
+	public byte[] generateBankDetailsPdf(String accountName, String bsbNumber, String accountNumber, String financialInstName, byte[] signatureImageBytes) throws Exception {
 		Document document = new Document();
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		PdfWriter.getInstance(document, out);
 		document.open();
+		
+		Image signatureImage = Image.getInstance(signatureImageBytes);
+		signatureImage.scaleToFit(120f, 60f); // Resize to fit signature area
+		signatureImage.setAlignment(Image.ALIGN_LEFT);
+		
+		String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
 
 		Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 16);
 		document.add(new Paragraph("Cheque/Savings Account", font));
@@ -43,8 +58,10 @@ public class RegisterService {
 						+ "between you and ZUBER CAR RENTAL PTY LTD as set out in this Request and in your Direct Debit Request Service Agreement. \n\n",
 						FontFactory.getFont(FontFactory.HELVETICA, 12)
 				));
-		document.add(new Paragraph("Signature: ___________________________ \t Date: _________________________ \n\n"));
-		document.add(new Paragraph("Signature: ___________________________ \t Date: _________________________"));
+		//document.add(new Paragraph("Signature: "));
+		document.add(signatureImage);
+		document.add(new Paragraph("Signature:                      \t\t Date: "+ currentDate +" \n\n"));
+		//document.add(new Paragraph("Signature: _____________ \t Date: _________________________"));
 		document.add(new Paragraph("             If debiting from a joint bank account, both signatures are required."));
 
 		document.add(new Paragraph("Completed Application", font));
@@ -56,5 +73,24 @@ public class RegisterService {
 				+ "				          		Wiley park, NSW, Australia      2195"));
 		document.close();
 		return out.toByteArray();
-	}	   
+	}
+	
+	public void saveFile(Long userId, MultipartFile file, String docType) throws IOException {
+        String fileExtension = getFileExtension(file.getOriginalFilename());
+        String fileName = docType + "." + fileExtension;
+
+        Path userDir = Paths.get(baseUploadDir, String.valueOf(userId));
+        Files.createDirectories(userDir);
+
+        Path filePath = userDir.resolve(fileName);
+
+        // Save or overwrite existing file
+        Files.write(filePath, file.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    private String getFileExtension(String filename) {
+        String cleanName = StringUtils.cleanPath(filename);
+        int index = cleanName.lastIndexOf('.');
+        return (index > 0) ? cleanName.substring(index + 1) : "pdf";
+    }
 }
